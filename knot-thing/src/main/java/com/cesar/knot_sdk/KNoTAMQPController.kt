LucasCCabral/knot-thing-lib-android

Add data publish operation
Add data publidh operation for AMQP.

knot-thing/src/main/java/com/cesar/knot_sdk/KNoTAMQPController.kt
@@ -3,6 +3,7 @@ package com.cesar.knot_sdk
 import com.cesar.knot_sdk.knot_messages.KNoTThingAuth
 import com.cesar.knot_sdk.knot_messages.KNoTThingRegister
 import com.cesar.knot_sdk.knot_messages.KNoTThingUnregister
+import com.cesar.knot_sdk.knot_messages.KNoTThingUpdateData
 import com.cesar.knot_sdk.knot_messages.KNoTThingUpdateSchema
 import com.google.gson.Gson
 
@@ -40,6 +41,14 @@ class KNoTAMQPController( val kNoTAMQP: KNoTAMQP) : KNoTMessager {
             Gson().toJson(kNoTThingSchema),
             kNoTAMQP.EXCHANGE_NAME_CLOUD,
             kNoTAMQP.BINDING_KEY_SCHEMA_UPDATE
+        )
+    }
+
+    override fun publishData(kNoTThingUpdateData: KNoTThingUpdateData) {
+        kNoTAMQP.publish(
+            Gson().toJson(kNoTThingUpdateData),
+            kNoTAMQP.EXCHANGE_NAME_CLOUD,
+            kNoTAMQP.BINDING_KEY_DATA_PUBLISH
         )
     }
 

alla3/knot-android-sdk

knot-thing/src/main/java/com/cesar/knot_sdk/KNoTAMQPController.kt
@@ -4,6 +4,7 @@ import com.cesar.knot_sdk.knot_messages.KNoTMessageRegister
 import com.cesar.knot_sdk.knot_messages.KNoTMessageUnregister
 import com.cesar.knot_sdk.knot_messages.KNoTMessageAuth
 import com.cesar.knot_sdk.knot_messages.KNoTMessageUpdateSchema
+import com.cesar.knot_sdk.knot_messages.KNoTMessageUpdateData
 import com.google.gson.Gson
 
 /**
@@ -40,6 +41,14 @@ class KNoTAMQPController(val knotAMQP: KNoTAMQP) : KNoTMessager {
             Gson().toJson(knotMessageSchema),
             knotAMQP.EXCHANGE_NAME_CLOUD,
             knotAMQP.BINDING_KEY_SCHEMA_UPDATE
+        )
+    }
+
+    override fun publishData(knotMessageUpdateData: KNoTMessageUpdateData) {
+        knotAMQP.publish(
+            Gson().toJson(knotMessageUpdateData),
+            knotAMQP.EXCHANGE_NAME_CLOUD,
+            knotAMQP.BINDING_KEY_DATA_PUBLISH
         )
     }
 