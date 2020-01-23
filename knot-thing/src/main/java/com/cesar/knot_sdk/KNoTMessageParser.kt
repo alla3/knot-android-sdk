c1c4914235f351c86f4f969dc020628d3543f07d
Allysson Lukas <alla@cesar.org.br>
2020-01-23 00:58:51 -0300

    Add parser to data update request
    Add parser to 'data.update' binding key request. The parser is used
    for decoding southbound traffic messages and follows the API defined
    on knot-fog-connector repository which can be found at the
    https://github.com/CESARBR/knot-fog-connector.git website and using the
    commit c688237c4a046549d840c3bf82722e4d8f0b9c95 as reference.

---
+++ knot-thing/src/main/java/com/cesar/knot_sdk/KNoTMessageParser.kt
@@ -0,0 +1,78 @@
+package com.cesar.knot_sdk
+
+import com.cesar.knot_sdk.KNoTTypes.KNOT_VALUE_NULL
+import com.cesar.knot_sdk.KNoTTypes.KNOT_VALUE_TYPE_BOOL
+import com.cesar.knot_sdk.KNoTTypes.KNOT_VALUE_TYPE_FLOAT
+import com.cesar.knot_sdk.KNoTTypes.KNOT_VALUE_TYPE_INT
+import com.cesar.knot_sdk.KNoTTypes.KNOT_VALUE_TYPE_MAX
+import com.cesar.knot_sdk.KNoTTypes.KNOT_VALUE_TYPE_RAW
+import com.cesar.knot_sdk.knot_messages.KNoTMessageDataItem
+import com.cesar.knot_sdk.knot_messages.KNoTMessageUpdateData
+import com.google.gson.JsonParser
+
+/**
+ * This class parses southbound traffic json requests in usable kotlin objects.
+ */
+class KNoTMessageParser {
+
+    private val sensorHash = mutableMapOf<Int, Int>()
+    private val VALUE = "value"
+    private val DATA = "data"
+    private val ID = "id"
+    private val SENSOR_ID = "sensorId"
+    private val ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE =
+            "The KNoTMessageParser is only prepared to handle " +
+            "KNOT_VALUE_TYPE_INT, KNOT_VALUE_TYPE_FLOAT, " +
+            "KNOT_VALUE_TYPE_BOOL, " + "KNOT_VALUE_TYPE_RAW, KNOT_VALUE_NULL."
+
+    private fun getSensorType(sensorId : Int) : Int {
+        val sensorType = sensorHash[sensorId]
+
+        if (sensorType == null) return KNOT_VALUE_NULL
+
+        return sensorType
+    }
+
+    fun addSensor(key : Int, value : Int) {
+        if (value >= KNOT_VALUE_TYPE_MAX) {
+            throw IllegalArgumentException(ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE)
+        }
+        sensorHash[key] = value
+    }
+
+    fun parseDataUpdate(knotData : String) : KNoTMessageUpdateData {
+        val receivedJsonObject = JsonParser.parseString(knotData).asJsonObject
+        val id = receivedJsonObject.get(ID).asString
+        val dataJasonArray = receivedJsonObject.getAsJsonArray(DATA)
+        val data = mutableListOf<KNoTMessageDataItem>()
+        var type : Int
+        var sensorId : Int
+        dataJasonArray.forEach {
+            sensorId = it.asJsonObject.get(SENSOR_ID).asInt
+            type = getSensorType(sensorId)
+            when(type) {
+                KNOT_VALUE_TYPE_INT -> data.add(KNoTMessageDataItem(
+                    sensorId,
+                    it.asJsonObject.get(VALUE).asInt)
+                )
+
+                KNOT_VALUE_TYPE_FLOAT -> data.add(KNoTMessageDataItem(
+                    sensorId,
+                    it.asJsonObject.get(VALUE).asFloat)
+                )
+
+                KNOT_VALUE_TYPE_BOOL -> data.add(KNoTMessageDataItem(
+                    sensorId,
+                    it.asJsonObject.get(VALUE).asBoolean)
+                )
+
+                KNOT_VALUE_TYPE_RAW -> data.add(KNoTMessageDataItem(
+                    sensorId,
+                    it.asJsonObject.get(VALUE).asString)
+                )
+            }
+        }
+
+        return KNoTMessageUpdateData(id, data)
+    }
+}
