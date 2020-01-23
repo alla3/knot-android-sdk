LucasCCabral/knot-thing-lib-android

Add auth operation
Add authentication operation for AMQP.

knot-thing/src/main/java/com/cesar/knot_sdk/KNoTAMQPController.kt
@@ -1,5 +1,6 @@
 package com.cesar.knot_sdk
 
+import com.cesar.knot_sdk.knot_messages.KNoTThingAuth
 import com.cesar.knot_sdk.knot_messages.KNoTThingRegister
 import com.cesar.knot_sdk.knot_messages.KNoTThingUnregister
 import com.google.gson.Gson
@@ -22,6 +23,14 @@ class KNoTAMQPController( val kNoTAMQP: KNoTAMQP) : KNoTMessager {
             Gson().toJson(kNoTThingUnregister),
             kNoTAMQP.EXCHANGE_NAME_CLOUD,
             kNoTAMQP.BINDING_KEY_UNREGISTER
+        )
+    }
+
+    override fun authenticate(kNoTThingAuth: KNoTThingAuth) {
+        kNoTAMQP.publish(
+            Gson().toJson(kNoTThingAuth),
+            kNoTAMQP.EXCHANGE_NAME_CLOUD,
+            kNoTAMQP.BINDING_KEY_AUTHENTICATE
         )
     }
 

alla3/knot-android-sdk

knot-thing/src/main/java/com/cesar/knot_sdk/KNoTAMQPController.kt
@@ -2,6 +2,7 @@ package com.cesar.knot_sdk
 
 import com.cesar.knot_sdk.knot_messages.KNoTMessageRegister
 import com.cesar.knot_sdk.knot_messages.KNoTMessageUnregister
+import com.cesar.knot_sdk.knot_messages.KNoTMessageAuth
 import com.google.gson.Gson
 
 /**
@@ -22,6 +23,14 @@ class KNoTAMQPController(val knotAMQP: KNoTAMQP) : KNoTMessager {
             Gson().toJson(knotThingUnregister),
             knotAMQP.EXCHANGE_NAME_CLOUD,
             knotAMQP.BINDING_KEY_UNREGISTER
+        )
+    }
+
+    override fun authenticate(knotMessageAuth: KNoTMessageAuth) {
+        knotAMQP.publish(
+            Gson().toJson(knotMessageAuth),
+            knotAMQP.EXCHANGE_NAME_CLOUD,
+            knotAMQP.BINDING_KEY_AUTHENTICATE
         )
     }
 